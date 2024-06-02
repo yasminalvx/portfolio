9c4a81049cacb7d0d1c6702ec7542cf06adb20ac
Yasmin <yasilvalves@gmail.com>
2024-06-02 00:19:56 -0300

    feat: escopo contact me

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -4,13 +4,21 @@ import { EducationComponent } from './education/education.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.scss'],
   standalone: true,
-  imports: [CommonModule, HomeComponent, WorkComponent, EducationComponent, ProjectsComponent],
+  imports: [
+    CommonModule,
+    HomeComponent,
+    WorkComponent,
+    EducationComponent,
+    ProjectsComponent,
+    ContactComponent
+  ],
 })
 export class BodyComponent implements OnInit {
   constructor() {}
