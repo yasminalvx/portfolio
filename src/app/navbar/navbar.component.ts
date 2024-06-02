3b6b719c6af55f473d269bb41b254a15ecbadb6c
Yasmin <yasilvalves@gmail.com>
2024-06-02 01:11:37 -0300

    feat: ajuste navbar menu de acordo com a rolagem

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { TabMenuModule } from 'primeng/tabmenu';
 import { BtnSwitcherThemeComponent } from './btn-switcher-theme/btn-switcher-theme.component';
@@ -14,6 +14,19 @@ import { BtnSwitcherThemeComponent } from './btn-switcher-theme/btn-switcher-the
 export class NavbarComponent implements OnInit {
   items: MenuItem[] | undefined;
   activeItem: MenuItem | undefined;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(e: any) {
+    if (!document) return;
+    let elementPosition = (document.documentElement.scrollTop || document.body.scrollTop) + 1;
+    const sectionsStr = ['home', 'work', 'education', 'projects', 'contact'];
+    const sections = sectionsStr.map(section => document.getElementById(section)?.offsetTop || 0);
+    Object.entries(sections).forEach(([index, section]) => {
+      if (elementPosition >= section && (elementPosition < sections[+index + 1] || +index === sections.length - 1)) {
+        this.activeItem = this.items?.[+index];
+      }
+    });
+  }
 
   ngOnInit() {
     this.items = [
